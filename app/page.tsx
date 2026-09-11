b9fa28f51e0feef837dfea2064399857b2cf8b19
Austin-Tillotson <atillotson07@gmail.com>
2026-09-11 17:18:43 -0400

    convert starfield and planets into full absolute positioned background

--- app/page.tsx
+++ app/page.tsx
@@ -11,30 +11,32 @@ export default function Home() {
       <Header />
 
       <main>
-        <Planet
-          className="planet--top-left glow--pulse"
-          radius={80}
-          opacity={0.15}
-          size={70}
-          pulseDuration={10}
-        />
-        <Planet
-          className="planet--contact glow--pulse"
-          radius={200}
-          opacity={0.17}
-          size={100}
-          pulseDuration={15}
-        />
-        <Planet
-          className="planet--lower-title glow--pulse"
-          radius={40}
-          opacity={0.11}
-          size={40}
-          pulseDuration={5}
-          pulseSizeMin={1}
-          pulseSizeMax={1}
-        />
-        <StarField />
+        <div aria-hidden="true" className="space-scene">
+          <Planet
+            className="planet--top-left glow--pulse"
+            radius={80}
+            opacity={0.15}
+            size={70}
+            pulseDuration={10}
+          />
+          <Planet
+            className="planet--contact glow--pulse"
+            radius={200}
+            opacity={0.17}
+            size={100}
+            pulseDuration={15}
+          />
+          <Planet
+            className="planet--lower-title glow--pulse"
+            radius={40}
+            opacity={0.11}
+            size={40}
+            pulseDuration={5}
+            pulseSizeMin={1}
+            pulseSizeMax={1}
+          />
+          <StarField />
+        </div>
         <ShootingStarManager />
         <Hero />
 
