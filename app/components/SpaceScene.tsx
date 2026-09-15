a25b72643bb14d82c292829b6f128275f08ff917
Austin-Tillotson <atillotson07@gmail.com>
2026-09-14 21:18:05 -0400

    updated planets size and positioning

--- app/components/SpaceScene.tsx
+++ app/components/SpaceScene.tsx
@@ -6,27 +6,41 @@ export default function SpaceScene() {
   return (
     <div aria-hidden="true" className="space-scene">
       <Planet
-        className="planet--top-left glow--pulse"
-        radius={80}
+        className="planet--one glow--pulse"
+        radius={300}
         opacity={0.15}
-        size={70}
-        pulseDuration={10}
+        size={100}
+        pulseDuration={5}
       />
       <Planet
-        className="planet--contact glow--pulse"
+        className="planet--two glow--pulse"
         radius={200}
         opacity={0.17}
         size={100}
         pulseDuration={15}
       />
       <Planet
-        className="planet--lower-title glow--pulse"
+        className="planet--three glow--pulse"
         radius={40}
         opacity={0.11}
         size={40}
         pulseDuration={5}
         pulseSizeMin={1}
         pulseSizeMax={1}
+      />
+      <Planet
+        className="planet--four glow--pulse"
+        radius={60}
+        opacity={0.13}
+        size={55}
+        pulseDuration={8}
+      />
+      <Planet
+        className="planet--five glow--pulse"
+        radius={120}
+        opacity={0.1}
+        size={110}
+        pulseDuration={8}
       />
       <StarField />
       <ShootingStarManager />
