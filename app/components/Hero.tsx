4b93116e3572853bd18beb414dbe10053038919e
Austin-Tillotson <atillotson07@gmail.com>
2026-09-19 17:45:28 -0400

    added personal information under hro image

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -80,17 +80,23 @@ export default function Hero() {
   return (
     <section className="hero" id="hero">
       <div className="hero__content">
-        <div className="hero__image-container">
-          <Image
-            alt="Austin Tillotson"
-            className="hero__image"
-            height={2208}
-            priority
-            sizes="(max-width: 375px) 80vw, (max-width: 550px) 70vw, (max-width: 767px) 24rem, (max-width: 1200px) calc(44.44vw - 5.33rem), 28rem"
-            src="/self.jpg"
-            width={2944}
-          />
-        </div>
+        <figure className="hero__image-figure">
+          <div className="hero__image-container">
+            <Image
+              alt="Austin Tillotson"
+              className="hero__image"
+              height={2208}
+              priority
+              sizes="(max-width: 375px) 80vw, (max-width: 550px) 70vw, (max-width: 767px) 24rem, (max-width: 1200px) calc(44.44vw - 5.33rem), 28rem"
+              src="/self.jpg"
+              width={2944}
+            />
+          </div>
+          <figcaption className="hero__image-details">
+            <span className="hero__image-name">Austin Tillotson</span>
+            <span className="hero__image-location">Melbourne, FL</span>
+          </figcaption>
+        </figure>
 
         <div className="hero__copy">
           <div className="hero__title glow glow--ellipse" style={heroGlowStyle}>
